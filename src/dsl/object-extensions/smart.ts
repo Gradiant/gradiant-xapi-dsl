264fdc80175c2c57417081d4a6cb7cafa537c93e
Lorens León Parra <lleon@gradiant.org>
2018-12-19 07:30:49 +0100

    feat: add questions extensions

--- src/dsl/object-extensions/smart.ts
+++ src/dsl/object-extensions/smart.ts
@@ -1,4 +1,5 @@
 export namespace smart {
   export const attemptContext = 'https://w3id.org/xapi/smart/extension/object/attempt-context';
   export const studentResponse = 'https://w3id.org/xapi/smart/extension/object/student-response';
+  export const questions = 'https://w3id.org/xapi/smart/extension/object/questions';
 }
