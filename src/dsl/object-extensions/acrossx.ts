f185e3a409307aa6fb6af25058e9b04d65f98b44
Lorens León Parra <lleon@gradiant.org>
2019-02-19 08:40:03 +0100

    chore: typo

--- src/dsl/object-extensions/acrossx.ts
+++ src/dsl/object-extensions/acrossx.ts
@@ -1,3 +1,7 @@
 export namespace acrossx {
+  /**
+   * @deprecated Will be removed in the major release use {@link acrossx.supplementalInfo} instead
+   */
   export const suplementalInfo = 'https://w3id.org/xapi/acrossx/extensions/supplemental-info';
+  export const supplementalInfo = 'https://w3id.org/xapi/acrossx/extensions/supplemental-info';
 }
