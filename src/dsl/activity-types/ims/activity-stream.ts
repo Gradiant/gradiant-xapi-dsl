f5a8406b2db02680975e284593782ad35b5acd6d
Lorens León Parra <lleon@gradiant.org>
2019-03-07 12:17:56 +0100

    feat: add feedback activity types
    The added types are:
    
    * question: http://activitystrea.ms/schema/1.0/question
    * review: http://activitystrea.ms/schema/1.0/review
    * survey: http://id.tincanapi.com/activitytype/survey

--- src/dsl/activity-types/ims/activity-stream.ts
+++ src/dsl/activity-types/ims/activity-stream.ts
@@ -2,5 +2,7 @@ export namespace activityStream {
   export namespace v1 {
     export const audio = 'http://activitystrea.ms/schema/1.0/audio';
     export const video = 'http://activitystrea.ms/schema/1.0/video';
+    export const question = 'http://activitystrea.ms/schema/1.0/question';
+    export const review = 'http://activitystrea.ms/schema/1.0/review';
   }
 }
--- src/dsl/activity-types/tincan.ts
+++ src/dsl/activity-types/tincan.ts
@@ -2,4 +2,5 @@ export namespace tincan {
   export const discussion = 'http://id.tincanapi.com/activitytype/discussion';
   export const forumTopic = 'http://id.tincanapi.com/activitytype/forum-topic';
   export const forumReply = 'http://id.tincanapi.com/activitytype/forum-reply';
+  export const survey = 'http://id.tincanapi.com/activitytype/survey';
 }
