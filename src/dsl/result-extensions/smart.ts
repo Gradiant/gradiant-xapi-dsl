f3d2761776bee66a2953b5a6e868e248cd114bb9
Jose Vilas <jvilas@gradiant.org>
2019-07-10 14:24:40 +0200

    feat: Add new result extensions for smart

--- src/dsl/result-extensions/smart.ts
+++ src/dsl/result-extensions/smart.ts
@@ -2,4 +2,6 @@ export namespace smart {
   export const userAction = 'https://w3id.org/xapi/smart/extension/result/user-action';
   export const evaluationFeedback = 'https://w3id.org/xapi/smart/extension/result/evaluation-feedback';
   export const discussionMessage = 'https://w3id.org/xapi/smart/extension/result/discussion-message';
+  export const activityMode = 'https://w3id.org/xapi/smart/extension/result/activity-mode';
+  export const activityStatus = 'https://w3id.org/xapi/smart/extension/result/activity-status';
 }
