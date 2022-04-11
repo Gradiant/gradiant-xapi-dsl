df987648de54055b7685b7ce695fa457a3123495
Agustín Cañas <acanas@gradiant.org>
2022-04-11 17:38:31 +0200

    chore(): remove sync for index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-var dsl_1 = require("./dsl");
-exports.dsl = dsl_1.dsl;
-__export(require("./types"));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJzcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSw2QkFBNEI7QUFBbkIsb0JBQUEsR0FBRyxDQUFBO0FBQ1osNkJBQXdCIn0=
